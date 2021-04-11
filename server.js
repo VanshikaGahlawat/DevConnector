1d913f5bc8f4a8dd08cf1251d66f1a980e88ab67
VanshikaGahlawat <gahlawatv23@gmail.com>
2021-04-11 16:09:23 +0530

    profile model

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ connectDB();
 
 //Define routes
 app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.get("/", function(req,res){
     res.send("server running");
