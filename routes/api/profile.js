29cbd29a81a340b270d41e0e5462e6fd514d6a63
VanshikaGahlawat <gahlawatv23@gmail.com>
2021-04-13 16:59:43 +0530

    add github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router= express.Router();
 const {check, validationResult}= require("express-validator");
+const config= require('config');
+const request= require('request');
 const auth= require("../../middleware/auth");
 const Profile= require('../../models/Profile');
 const User= require('../../models/User');
@@ -254,5 +256,32 @@ router.delete('/education/:education_id', auth, async function(req,res){
         
     }
 });
+
+//@route GET api/profile/github/:username
+//@desc Get github repos
+//@access Public
+router.get('/github/:username', function(req,res){
+    try {
+        options ={
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientid')}&client_secret=${config.get('githubSecret')}` ,
+            method: 'GET',
+            headers: {'user-agent':'node.js' }
+        }
+
+        request(options,(error, response, body) =>{
+            if(error) console.error(error.message)
+
+            if(response.statusCode !== 200){
+                return res.status(404).json({msg:"github not found"});
+            }
+
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('sever error');
+        
+    }
+})
 
 module.exports = router; 
