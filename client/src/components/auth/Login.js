500f810cf53d5f5cc2970c0fecaad531ed09fe5f
VanshikaGahlawat <gahlawatv23@gmail.com>
2021-04-18 18:16:01 +0530

    react auth-login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,11 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
+import auth from '../../reducers/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData]= useState({
         email:'',
         password:''
@@ -10,20 +14,24 @@ const Login = () => {
     const {email, password}= formData;
 
     const onChange= e => {
-        setFormData({...formData, [e.target.name]: e.target.value});
+        setFormData({ ...formData, [e.target.name] : e.target.value});
+        
     }
     const onSubmit= e => {
         e.preventDefault();
-        console.log(formData);
+        login(email,password);
     }
 
+    if(isAuthenticated){
+        return <Redirect to='/dashboard' />
+    }
     return (
         <Fragment>
             <h1 className="large text-primary">Sign In</h1>
             <p className="lead"><i className="fas fa-user"></i> Login Your Account</p>
             <form className="form" onSubmit= {e => onSubmit(e)}>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value={email} onChange= {e => onChange(e)} required/>
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange= {e => onChange(e)} />
                 </div>
                 <div className="form-group">
                     <input
@@ -32,7 +40,7 @@ const Login = () => {
                         name="password"
                         value={password}
                         onChange= {e => onChange(e)}
-                        minLength="8"
+                        
                     />
                 </div>
                 
@@ -45,4 +53,13 @@ const Login = () => {
     );
 }
 
-export default Login;
+Login.propTypes={
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setAlert} from "../../actions/alert";
-import PropTypes from 'prop-types'
+import {register} from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register, isAuthenticated}) => {
  //above props are destuctured, instead of using props.setAlert we can now use just setAlert    
     const [formData, setFormData]= useState({
         name: '',
@@ -25,7 +26,13 @@ const Register = ({setAlert}) => {
         if(password !== password2){
             setAlert('Passwords do not match', 'danger');
         }
-        else console.log('Success');
+        else {
+            register({name,email,password});
+        }
+    }
+
+    if(isAuthenticated){
+        return <Redirect to="/dashboard" />
     }
 
     return (
@@ -69,7 +76,13 @@ const Register = ({setAlert}) => {
 };
 
 Register.propTypes={
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
 }
 
-export default connect(null, {setAlert})(Register);
+const mapStateToProps = state =>({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {setAlert, register})(Register);
