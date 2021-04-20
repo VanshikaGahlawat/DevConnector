8c5b4d49582792c0a69699c50f4b61f5e6ac3122
VanshikaGahlawat <gahlawatv23@gmail.com>
2021-04-20 19:11:43 +0530

    create profile

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
 
     const authLinks=(
 		<ul>
-          <li><a href="!#" onClick={logout}>
-		  <i className='fas fa-sign-out-alt'></i>{' '}
-		  <span className='hide-sm'>Logout</span></a></li>
-          <li><Link to="/dashboard"><i className='fas fa-user'></i>{' '}<span>Dashboard</span></Link></li>
+        <li><Link to="/dashboard"><i className='fas fa-user'></i>{' '}<span>Dashboard</span></Link></li>
+        <li><a href="#!" onClick={logout}>
+		    <i className='fas fa-sign-out-alt'></i>{' '}
+		    <span className='hide-sm'>Logout</span></a></li>
           {/* <li><Link to="/login">Login</Link></li> */}
         </ul>
 	);
